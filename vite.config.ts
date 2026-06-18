a3b816aea4fef820d1dc879f1571bf6ee1e1b3d0
lks <1471519969@qq.com>
2026-06-18 15:33:07 +0800

    fix: 配置GitHub Pages子目录路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [uni()],
+  base: '/zuopinguanlixitongXCX/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
