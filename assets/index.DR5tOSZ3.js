9d9c770e78df08efbf632e13688f9da4c3e394d5
lks <1471519969@qq.com>
2026-06-18 16:03:27 +0800

    deploy: 更新H5构建产物

---
+++ assets/index.DR5tOSZ3.js
@@ -0,0 +1,1 @@
+function t(t){const e="string"==typeof t?new Date(t):t;return`${e.getFullYear()}-${String(e.getMonth()+1).padStart(2,"0")}-${String(e.getDate()).padStart(2,"0")}`}export{t as f};
---
+++ assets/pageTransition.C0qfk19R.js
@@ -0,0 +1,1 @@
+import{r as e,o as r}from"./index-DT5a2nzo.js";function a(){const a=e(!1);return r(()=>{requestAnimationFrame(()=>{requestAnimationFrame(()=>{a.value=!0})})}),{pageVisible:a}}export{a as u};
